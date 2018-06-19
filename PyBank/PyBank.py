d0293ea151413d99fb8d74e501f57aa425f03597
JCCom <Jccommander@Johns-MacBook-Pro-2.local>
2018-06-19 14:03:10 -0400

    Added new file paths to better fit into the framework of the homework; added the CSV files to the repository

---
+++ PyBank/PyBank.py
@@ -0,0 +1,107 @@
+# Goals: Create script that takes CSV file and counts total number of months in the dataset, total net
+# amount of profit/losses, the average change in profit/losses between months, the greatest increase in
+# profits (date and amount) over the entire period, and the greatest decrease in losses (date and amount)
+
+# The CSV data set is a series of rows organized by two columns, one containing dates, and the other
+# containing sets of numbers representing profits / losses, assumption is that the dataset will always
+# be organized by date in chronological order
+
+# Import os and csv modules for use
+
+import os
+import csv
+
+# Path to the CSV file
+
+budgetpath = os.path.join('..','CSV Files','budget_data.csv')
+
+# Open the CSV file, and set it to read
+
+with open(budgetpath, newline='') as budgetfile:
+    budgetreader = csv.reader(budgetfile, delimiter=',')
+
+    # Skip the header in the dataset
+
+    next(budgetreader)
+
+# Set variable for total months, total profits, greatest increase, greatest decrease and previous value to
+# zero
+
+    totalmonths = 0
+    totalprofits = 0
+    greatest_inc = 0
+    greatest_dec = 0
+    prev_val = 0
+
+# Create an empty list to store changes month over month in order to create an average
+
+    avglist = []
+
+    # Count the total number of months in the dataset
+
+    for row in budgetreader:
+        totalmonths = totalmonths + 1
+
+    # Add the total profits and losses together
+
+        totalprofits = totalprofits + int(row[1])
+
+    # Average the profits and losses
+        # First, capture the current row value
+        curr_val = int(row[1])
+        # Append the average list with the current value minus the previous value
+        avglist.append(curr_val - prev_val)
+        # Set the previous value to the current row, so it's ready for the next loop
+        prev_val = int(row[1])
+
+    # Find the greatest increase in profits
+        # Create a conditional that checks if the current value is bigger than the previous stored
+        # value. If it is, then commit that value to the variable
+        if int(row[1]) > greatest_inc:
+            greatest_inc = int(row[1])
+            great_inc_string = row[0]
+
+    # Find the greatest decrease in profits
+        # Create a conditional that checks if the current value is smaller than the previous stored
+        # value. If it is, then commit that value to the variable
+        if int(row[1]) < greatest_dec:
+            greatest_dec = int(row[1])
+            great_dec_string = row[0]
+
+# Finish the average change over months by taking the average list, summing it, then dividing it by the
+# total number of items in the list
+
+final_avg = round(sum(avglist) / len(avglist),2)
+
+# Print each resulting number to the terminal with descriptions and formatting
+
+print("Financial Analysis")
+print("------------------------------")
+print(f"Total Months: {totalmonths}")
+print(f"Total: ${totalprofits}")
+print(f"Average Change: ${final_avg}")
+print(f"Greatest Increase in Profits: {great_inc_string} (${greatest_inc})")
+print(f"Greatest Decrease in Profits: {great_dec_string} (${greatest_dec})")
+
+# Write a new text file
+    
+    # Set an output path for the text file
+
+output_path = os.path.join(".", "Financial-Analysis.csv")
+
+    # Create and open the new text file
+
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Set the file to write
+
+    financewriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the individual lines to be printed to the text file
+    financewriter.writerow("Financial Analysis")
+    financewriter.writerow("------------------------------")
+    financewriter.writerow(f"Total Months: {totalmonths}")
+    financewriter.writerow(f"Total: ${totalprofits}")
+    financewriter.writerow(f"Average Change: ${final_avg}")
+    financewriter.writerow(f"Greatest Increase in Profits: {great_inc_string} (${greatest_inc})")
+    financewriter.writerow(f"Greatest Decrease in Profits: {great_dec_string} (${greatest_dec})")
---
+++ PyPoll/PyPoll.py
@@ -0,0 +1,119 @@
+# Goal is to take a list of election votes, and return the total number of votes cast, a complete list of
+# candidates who received votes, the percentage of votes each candidate won, the total number of votes
+# each candidate won, and the winner of the election based on popular vote
+
+# The dataset is over a million rows, with three fixed columns, one listing a voter ID, one listing a county
+# and the last listing which candidate the voter voted for
+
+# Import modules for use
+
+import os
+import csv
+
+# Create path to CSV file
+
+electionpath = os.path.join('..', 'CSV Files', 'election_data.csv')
+
+# Create variable for total votes and the winner, both set to 0
+
+total_vote = 0
+winner = 0
+
+# Create two dictionaries, one to store the individual candidates and their votes, and the other to hold
+# the candidates percentage values; also create a list for candidate names
+
+can_dict = {}
+percent_dict = {}
+can_list = []
+
+# Create function to count the votes for individual candidates
+
+def voteCounter(candidate):
+
+    # First, the function should check to see if a candidate is present in the dictionary, if not
+    # then the candidate should be added to the dictionary with a starting value of 1
+
+    if row[2] not in can_dict:
+        can_dict[row[2]] = 1
+    
+    # If the candidate is already present, then their tally should be increased by 1
+
+    else:
+        can_dict[row[2]] = can_dict[row[2]] + 1
+
+# Open and read the CSV file
+
+with open(electionpath, newline='') as electionfile:
+    electionreader = csv.reader(electionfile, delimiter=',')
+
+    # Skip the header of the dataset
+
+    next(electionreader)
+
+    # Iterate through the spreadsheet, counting each vote and running the vote counter function on each row
+    # Then, it captures the individual candidate names so they can be matched with their count later
+
+    for row in electionreader:
+        total_vote = total_vote + 1
+        voteCounter(row)
+        if row[2] not in can_list:
+            can_list.append(row[2])
+        
+    # Calculate the percentage of votes won by a candidate
+        # Iterate through the now completed dictionary, performing a percentage equation for each candidate.
+        # These percentage values should be stored in the percent dictionary
+
+    for candidate in can_dict:
+        percent_dict[candidate] = (round(can_dict[candidate] / total_vote * 100,2))
+
+    # Also during this iteration, a conditional should be made that compares the total vote tally of each
+    # candidate to see who has the most points, storing the highest value in the winner variable, and the
+    # candidate string in a new variable called winner_str so that it can be called later
+        if can_dict[candidate] > winner:
+            winner = can_dict[candidate]
+            winner_str = candidate
+
+# Print results with formatting
+
+    # To print the list of candidates, percentages and total votes, use a for loop to iterate for as many
+    # candidates that are present
+
+print("Election Results")
+print("---------------------------")
+print(f"Total Votes: {total_vote}")
+print("---------------------------")
+
+for candidate in can_list:
+    print(candidate + ": " + str(percent_dict[candidate]) + "% (" + str(can_dict[candidate]) + ")")
+
+print("---------------------------")
+print(f"Winner: {winner_str}")
+print("---------------------------")
+
+# Export results in a text file
+
+    # Set an output path for the text file
+
+output_path = os.path.join(".", "Election-Results.csv")
+
+    # Create and open the new text file
+
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Set the file to write
+
+    electionwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the individual lines to be printed to the text file
+
+    electionwriter.writerow("Election Results")
+    electionwriter.writerow("---------------------------")
+    electionwriter.writerow(f"Total Votes: {total_vote}")
+    electionwriter.writerow("---------------------------")
+
+    for candidate in can_list:
+        electionwriter.writerow(candidate + ": " + str(percent_dict[candidate]) + "% (" + str(can_dict[candidate]) + ")")
+
+    electionwriter.writerow("---------------------------")
+    electionwriter.writerow(f"Winner: {winner_str}")
+    electionwriter.writerow("---------------------------")
